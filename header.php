2addf36e0db775582dd1b56abdf3eca0f3b8259d
CutyDog <72608417+CutyDog@users.noreply.github.com>
2021-02-12 02:51:03 +0900

    Create header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+<title>株式会社ZEROGRA</title>
+	<meta name="description" content="既成概念をゼロにして（＝zerogravity）、新しい常識をつくりだす">
+<!--css-->
+	<link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); echo '?' . filemtime( get_stylesheet_directory() . '/style.css'); ?>" type="text/css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <?php wp_head(); ?>
+</head>
+
