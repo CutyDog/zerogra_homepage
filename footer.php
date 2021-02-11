613293e9b8aa538a1f3a113ee23d628224fc73f0
CutyDog <72608417+CutyDog@users.noreply.github.com>
2021-02-12 02:51:30 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+    <footer>
+		<div class="end">
+		<a href="<?php echo home_url(); ?>"><img class="logophoto" src="<?php echo get_template_directory_uri(); ?>/images/zerogra_logo_main_bg_tomei.png" alt="zerogra"></a>
+        <div class="ends">&copy; Zerogra All Rights Reserved.</div>
+    </footer>
+	<?php wp_footer(); ?>	
+</body>
